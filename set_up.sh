3fd5db31f6d59b342c50634aacd0d8eb8bc89cd8
Saúl Axel <saulaxel.50@gmail.com>
2017-02-12 22:42:47 -0600

    Automatic setup script improved

--- set_up.sh
+++ set_up.sh
@@ -10,5 +10,6 @@ cp ./vim/colors/solarized.vim ~/.vim/colors;
 cp ./vimrc ~/.vimrc;
 cp ./bashrc ~/.bashrc;
 cp ./nanorc ~/.nanorc;
+cp ./vim/snips/*.snip ~/.vim/bundle/neosnippet-snippets/neosnippets
 
 exit 0;
