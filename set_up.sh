eecc8aabfedfcebbd4fc48b628a713fe5a2b4440
Saúl Axel <saulaxel.50@gmail.com>
2017-02-13 20:57:38 -0600

    The set_up script uninstalls unused plugins

--- set_up.sh
+++ set_up.sh
@@ -23,6 +23,6 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi;
 
-vim +BundleInstall +qall
+vim +PluginInstall +PluginClean +qall
 
 exit 0;
