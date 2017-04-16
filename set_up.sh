a9b49c6d99eeaa94c46e6ad1c95deeee746bcea9
Live session user <saulaxel.50@gmail.com>
2017-04-16 06:09:14 +0000

    Support for mint

--- set_up.sh
+++ set_up.sh
@@ -2,11 +2,11 @@
 
 # Set the package manager
 
-if [ $(uname -r | grep -i 'ubuntu') ]; then
+if [ "$(uname -a | grep -i 'ubuntu')" ]; then
     pkg_man="apt-get install"
-elif [ $(uname -r | grep -i 'arch') ]; then
+elif [ "$(uname -a | grep -i 'arch')" ]; then
     pkg_man="pacman -S"
-elif [ $(uname -r | grep -i 'fedora') ]; then
+elif [ "$(uname -a | grep -i 'fedora')" ]; then
     pkg_man="yum install"
 fi
 
@@ -20,13 +20,13 @@ fi
 
 # Install dependencies
 if ! hash vim 2>/dev/null; then
-    sudo $pkg_man vim;
-    sudo $pkg_man vim-nox;
+    sudo $pkg_man vim
+    sudo $pkg_man vim-nox
 fi;
 
 
 if ! hash nano 2>/dev/null; then
-    sudo $pkg_man nano;
+    sudo $pkg_man nano
 fi;
 
 #sudo apt-get install tmux;
@@ -42,8 +42,8 @@ fi
 
 # Starts real set-up
 if [ ! -d ~/.vim/bundle ] || [ ! -d ~/.vim/colors ]; then
-    mkdir -p ~/.vim/bundle;
-    mkdir ~/.vim/colors;
+    mkdir -p ~/.vim/bundle
+    mkdir ~/.vim/colors
 fi;
 
 cp ./vimrc ~/.vimrc;
