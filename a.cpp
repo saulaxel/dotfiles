a25e58fc328b60af396d02d36344c21d9ffcfb05
Saúl Axel <saulaxel.50@gmail.com>
2017-05-08 13:18:23 -0500

    cpp snippets

---
+++ a.cpp
@@ -0,0 +1,13 @@
+class EstaEsMiClase {
+    public:
+        EstaEsMiClase(int a, int b);
+        ~EstaEsMiClase(void);
+        int a, b;
+    private:
+};
+
+EstaEsMiClase::EstaEsMiClase(int a, int b) {
+}
+
+EstaEsMiClase::~EstaEsMiClase(void) {
+}
