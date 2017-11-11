e96f98a2f64ac03910084ff6ec272a738a5bfdd4
Saul Axel <saulaxel@gmail.com>
2017-11-10 19:32:54 -0600

    Processing args

--- set_up.sh
+++ set_up.sh
@@ -1,13 +1,53 @@
 #!/bin/bash
 
-# Set the package manager
+# Process args
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -v|--vim)
+            IVIM=1
+            ;;
 
+        -nv|--neovim)
+            INVIM=1
+            ;;
+
+        -r|--ruby)
+            IRUBY=1
+            ;;
+
+        -t|--tmux)
+            ITMUX=1
+            ;;
+
+        -c|--clang)
+            ICLANG=1
+            ;;
+
+        -b|--backup)
+            BACKUP=1
+            ;;
+
+        -a|--all)
+            IVIM=1
+            INVIM=1
+            IRUBY=1
+            ITMUX=1
+            BACKUP=1
+            ;;
+        *)
+    esac
+    shift
+done
+
+# Set the package manager
 if [ "$(uname -a | grep -i 'ubuntu')" ]; then
     pkg_man="apt-get install"
 elif [ "$(uname -a | grep -i 'arch')" ]; then
     pkg_man="pacman -S"
 elif [ "$(uname -a | grep -i 'fedora')" ]; then
     pkg_man="yum install"
+else
+    pkg_man="choco install"
 fi
 
 # Set the shell config file
@@ -19,23 +59,21 @@ elif [ $(echo $SHELL | grep -i zsh ) ]; then
 fi
 
 # Install dependencies
-if ! hash vim 2>/dev/null; then
-    sudo $pkg_man vim
-    sudo $pkg_man vim-nox
+if [ -n "$IVIM" ]; then
+    $pkg_man vim
+    $pkg_man vim-nox
 fi
 
-if ! hash nano 2>/dev/null; then
-    sudo $pkg_man nano
+if [ -n "$IRUBY" ]; then
+    $pkg_man ruby
 fi
 
-if ! hash gem 2>/dev/null; then
-    sudo $pkg_man ruby; sudo gem install sass
+if [ -n "$ITMUX" ]; then
+    $pkg_man tmux
 fi
-
-#sudo apt-get install tmux;
 
 # Make a back-up
-if [ ! -d ~/.dotfiles_backup ]; then
+if [ -n "$BACKUP" ]; then
     mkdir ~/.dotfiles_backup
 
     cp ~/.vimrc ~/.dotfiles_backup
@@ -49,29 +87,43 @@ if [ ! -d ~/.vim/bundle ] || [ ! -d ~/.vim/colors ]; then
     mkdir ~/.vim/colors
 fi;
 
-cp ./vimrc ~/.vimrc;
+cp ./vimrc ~/.vimrc
+
+if [ -n "$INVIM" ]; then
+    if [ ! -d ~/.config/nvim ]; then
+        mkdir -p ~/.config/nvim
+    fi
+    ln -n .vimrc ~/.config/nvim/init.vim
+fi
 
 if [ "$(cat $shell | grep "BASH_CONFIG_INCLUDED")" == "" ]; then
     cat bashrc >> $shell
 fi
 
-cp ./nanorc ~/.nanorc;
+cp ./nanorc ~/.nanorc
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi;
 
-vim +PluginInstall +qall
+if [ -n "$IVIM" ]; then
+    vim +PluginInstall +qall
+fi
+
+if [ -n "$INVIM" ]; then
+    nvim +PluginInstall +qall
+fi
 
 cp ./vim/snips/emmet.vim ~/.vim/bundle/emmet-vim/autoload
 cp ./vim/snips/*.snip ~/.vim/bundle/neosnippet-snippets/neosnippets
 cp ./vim/colors/solarized.vim ~/.vim/colors
 
-if [ ! -d ~/vimproc ]; then
-    sudo $pkg_man clang
-    git clone https://github.com/Shougo/vimproc.vim ~/vimproc
+if [ -n "$ICLANG" ]; then
+    $pkg_man clang
 
-    vim +VimProcInstall +qall
+    if [ -n "$INVIM" ]; then
+        vim +VimProcInstall +qall
+    fi
 fi
 
-exit 0;
+exit 0
